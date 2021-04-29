7674142a7ef20e7becf0f8dd9bc7aa96f99bfb29
Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>
2021-04-29 08:56:14 +0100

    PZ-76 | ArchiveUtil Unit Test + Implemented unit test for some core cases. + Updated condition on extractToDirectory to better cater for negative scenarios.
    Signed-off-by: Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>

--- pearl-zip-ui/src/test/java/com/ntak/pearlzip/ui/UITestSuite.java
+++ pearl-zip-ui/src/test/java/com/ntak/pearlzip/ui/UITestSuite.java
@@ -8,6 +8,10 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /***
  *   @author akakshepati
  */
@@ -15,4 +19,20 @@ import org.junit.runner.RunWith;
 @SelectPackages("com.ntak.pearlzip")
 @SuiteDisplayName("PearlZip UI module tests")
 public class UITestSuite {
+
+    public static void clearDirectory(Path directory) throws IOException {
+        Files.walk(directory).filter((f)->!Files.isDirectory(f)).forEach(f-> {
+            try {
+                Files.deleteIfExists(f);
+            } catch(IOException e) {
+            }
+        });
+        Files.walk(directory).filter(Files::isDirectory).sorted((a,b)->b.toString().length()-a.toString().length()).forEach(f-> {
+            try {
+                Files.deleteIfExists(f);
+            } catch(IOException e) {
+            }
+        });
+        Files.deleteIfExists(directory);
+    }
 }
